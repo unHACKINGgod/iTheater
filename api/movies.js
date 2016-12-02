abec3b8c8b2e4cad1fe392489682d39e74713d43
Agboola Olasunkanmi <Agboola Olasunkanmi>
2016-12-02 18:00:19 +0300

    we did a great job with the template and database

--- api/movies.js
+++ api/movies.js
@@ -3,22 +3,22 @@ var movieslist1={
   type:"Action, Drama",
   discription:"raymond reddinton list of underworld criminals.",
   ratings:5,
-  posterImage: "./img/blacklist.png",
+  posterImage: "blacklist.png",
 };
 var movieslist2={
   name:"Suits",
   type:"Thriler, Drama",
   discription:"Great law firm",
   ratings:4,
-  posterImage: "./img/suits.jpg",
-  
+  posterImage: "suits.jpg",
+
 };
 var movieslist3={
   name:"Ray Donavan",
   type:"Thriler, Drama",
   discription:"LA Fixer",
   ratings:5,
-  posterImage: "./img/ray.jpg",
+  posterImage: "ray.jpg",
 };
 var movies = [movieslist1,movieslist2,movieslist3];
 module.exports=movies;
--- index.js
+++ index.js
@@ -1,7 +1,47 @@
 var exp = require('express');
+var mong= require('mongoose');
+mong.connect('mongodb://unc:landmine@ds117348.mlab.com:17348/itheater')
+var Cat = mong.model('Cat', { name: String });
+
+var kitty = new Cat({ name: 'unc' });
+kitty.save(function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('meow');
+  }
+});
+
+// design
+var movie = mong.model('movies',{name: String, type: String, ratings: String});
+
+// implementation
+var movie1=new movie({name: "MOvie1", type:"horro", ratings:10});
+
+movie1.save(function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('meow');
+  }
+});
+
+var reg2 = {name: "unc"}
+var reg = mong.model('registration',{name: String, email:String, password: String});
+
+var user = new reg({name: "unc", email:"unc@delandev.com", passwor:"landmine"});
+
+user.save(function(err){
+  if (err) {
+    console.log("Oooh" + reg2.name + "you have no idea what you doing");
+  }else{
+    console.log("Good Job Boy");
+  }
+});
 var moviedata = require('./api/movies.js');
 app= exp();
 app.set('view engine', 'pug')
+// mongodb://<dbuser>:<dbpassword>@ds117348.mlab.com:17348/itheater
 console.log(moviedata);
 var Router = exp.Router();
 Router.get('/city/:moscow', function(req,res){
@@ -9,6 +49,7 @@ Router.get('/city/:moscow', function(req,res){
   res.render('index',{moscow: 'this ' +seasionname+', is good', moviedataInsideTemplate: moviedata})
 });
 app.use(Router);
+app.use('/public', exp.static('public'));
 app.listen(5000,function(){
   console.log('I\'m alive');
 });
