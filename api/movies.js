00c70efd1f47dc9a7f2278857fce8ca35d9f01a8
Agboola Olasunkanmi <Agboola Olasunkanmi>
2016-12-03 06:14:51 +0300

    designing and data collection.

--- api/movies.js
+++ api/movies.js
@@ -1,7 +1,7 @@
 var movieslist1={
   name:"Blacklist",
   type:"Action, Drama",
-  discription:"raymond reddinton list of underworld criminals.",
+  discription:"raymond reddinton list of underworld criminals:",
   ratings:5,
   posterImage: "blacklist.png",
 };
@@ -19,6 +19,189 @@ var movieslist3={
   discription:"LA Fixer",
   ratings:5,
   posterImage: "ray.jpg",
-};
-var movies = [movieslist1,movieslist2,movieslist3];
+}
+var movieslist4={
+  name:"Jackie (2016)",
+Certificate:"R 99 min   -   Biography | Drama",
+discription: "Following the assassination of President John F. Kennedy, First Lady Jacqueline Kennedy fights through grief and trauma to regain her faith, console her children, and define her husband's historic legacy.",
+Director: "Pablo Larraín",
+Stars: "Natalie Portman, Peter Sarsgaard, Greta Gerwig, Billy Crudup",
+}
+
+var movieslist5={
+  name: "Incarnate (2016)",
+  Certificate: "PG-13 91 min   -   Horror | Thriller",
+  discription:"A scientist with the ability to enter the subconscious minds of the possessed must save a young boy from the grips of a demon with powers never seen before, while facing the horrors of his past.",
+Director: "Brad Peyton",
+Stars: "Carice van Houten, Aaron Eckhart, David Mazouz, Catalina Sandino Moreno",
+}
+
+var movieslist6={
+name:"The Eyes of My Mother (2016) - [Limited]",
+Certificate: "R 76 min   -   Drama | Horror",
+discription: "A young, lonely woman is consumed by her deepest and darkest desires after tragedy strikes her quiet country life.",
+Director: "Nicolas Pesce",
+Stars: "Kika Magalhaes, Will Brill, Olivia Bond, Paul Nazak",
+}
+
+var movieslist7={
+  name:"Будущее (2016) - [Limited]",
+  Certificate: " PG-13 102 min   -   Drama",
+  discription: "A philosophy teacher soldiers through the death of her mother, getting fired from her job, and dealing with a husband who is cheating on her.",
+  Director: "Mia Hansen-Løve",
+  Stars: "Isabelle Huppert, André Marcon, Roman Kolinka, Edith Scob",
+}
+
+var movieslist8={
+  name: "Old Stone (2016) - [Limited]",
+Certificate :""Unrated 80 min"  - ""Thriller",
+discription :"A psychological thriller about a taxi driver battling bureaucracy and legal manipulation in China.",
+Director: "Johnny Ma",
+Stars: "Gang Chen, Nai An, Hongwei Wang, Zebin Zhang",
+}
+
+var movieslist9={
+  name:"Дуэлянт (2016) - [Limited]",
+certificate: "R 109 min   -   Action | Drama | Thriller",
+discription: "Set in 19th century Imperial Russia, the story revolves around a retired army veteran \
+who makes a good living by settling duels for aristocrats, a common practice of the era. Shot in IMAX. \
+Cast Petr Fedorov, Vladimir Mashkov, Martin Wuttke, Julia Khlynina, Yuri Kolokolnikov Director Alexey Mizgirev",
+Director: "Aleksey Mizgirev",
+Stars: "Pyotr Fyodorov, Vladimir Mashkov, Yuliya Khlynina, Yuri Kolokolnikov",
+}
+
+
+var movieslist10={
+  name:"Moana (2016)",
+Certificate :"PG 103 min   -   Animation | Adventure | Comedy | Family | Fantasy | Musical",
+ratngs: 8.3,
+discription: "In Ancient Polynesia, when a terrible curse incurred by Maui reaches an impetuous\
+ Chieftain's daughter's island, she answers the Ocean's call to seek out the demigod to set things right.",
+Directors: "Ron Clements | Don Hall | John Musker | Chris Williams",
+Stars: "Auli'i Cravalho, Dwayne Johnson, Rachel House, Temuera Morrison",
+}
+var movieslist4={
+  name:"Fantastic Beasts and Where to Find Them (2016)"
+  Certificate:" PG-13 133 min   -   Adventure | Family | Fantasy",
+  ratings:7.8,
+  Metascore: 66/100 (50 reviews)
+The adventures of writer Newt Scamander in New York's secret community of witches and wizards seventy years before Harry Potter reads his book in school.
+Director: David Yates
+Stars: Eddie Redmayne, Katherine Waterston, Alison Sudol, Dan Fogler
+Box Office: Weekend: $74.40M, Gross: $164.96M
+Watch Trailer Add to Watchlist
+Doctor Strange (2016) Poster
+
+Doctor Strange (2016)
+
+Certificate PG-13 115 min   -   Action | Adventure | Fantasy | Sci-Fi
+
+7.9/10
+Metascore: 72/100 (49 reviews)
+A former neurosurgeon embarks on a journey of healing only to be drawn into the world of the mystic arts.
+Director: Scott Derrickson
+Stars: Benedict Cumberbatch, Chiwetel Ejiofor, Rachel McAdams, Benedict Wong
+Box Office: Weekend: $85.06M, Gross: $208.82M
+Watch Trailer Add to Watchlist
+Allied (2016) Poster
+
+Allied (2016)
+
+Certificate R 124 min   -   Action | Drama | Romance | Thriller | War
+
+7.2/10
+Metascore: 60/100 (44 reviews)
+In 1942, an intelligence officer in North Africa encounters a female French Resistance fighter on a deadly mission behind enemy lines. When they reunite in London, their relationship is tested by the pressures of war.
+Director: Robert Zemeckis
+Stars: Brad Pitt, Marion Cotillard, Jared Harris, Vincent Ebrahim
+Box Office: Weekend: $12.70M, Gross: $21.88M
+Watch Trailer Add to Watchlist
+Arrival (2016) Poster
+
+Arrival (2016)
+
+Certificate PG-13 116 min   -   Drama | Mystery | Sci-Fi | Thriller
+
+8.4/10
+Metascore: 81/100 (52 reviews)
+A linguist is recruited by the military to assist in translating alien communications.
+Director: Denis Villeneuve
+Stars: Amy Adams, Jeremy Renner, Forest Whitaker, Michael Stuhlbarg
+Box Office: Weekend: $24.00M, Gross: $65.78M
+Watch Trailer Add to Watchlist
+Trolls (2016) Poster
+
+Trolls (2016)
+
+Certificate PG 92 min   -   Animation | Adventure | Comedy | Family | Fantasy | Musical
+
+6.7/10
+Metascore: 56/100 (32 reviews)
+After the Bergens invade Troll Village, Poppy, the happiest Troll ever born, and the curmudgeonly Branch set off on a journey to rescue her friends.
+Directors: Walt Dohrn | Mike Mitchell
+Stars: Anna Kendrick, Justin Timberlake, Zooey Deschanel, Christopher Mintz-Plasse
+Box Office: Weekend: $46.58M, Gross: $136.77M
+Watch Trailer Add to Watchlist
+Bad Santa 2 (2016) Poster
+
+Bad Santa 2 (2016)
+
+Certificate R 92 min   -   Comedy | Crime | Drama
+
+5.7/10
+Metascore: 38/100 (36 reviews)
+Fueled by cheap whiskey, greed and hatred, Willie teams up once again with his angry little sidekick, Marcus, to knock off a Chicago charity on Christmas Eve.
+Director: Mark Waters
+Stars: Billy Bob Thornton, Kathy Bates, Tony Cox, Christina Hendricks
+Box Office: Weekend: $6.18M, Gross: $11.00M
+Watch Trailer Add to Watchlist
+Almost Christmas (2016) Poster
+
+Almost Christmas (2016)
+
+Certificate PG-13 111 min   -   Comedy | Drama
+
+6/10
+Metascore: 55/100 (27 reviews)
+A dysfunctional family gathers together for their first Christmas since their mom died.
+Director: David E. Talbert
+Stars: Kimberly Elise, Omar Epps, Danny Glover, Mo'Nique
+Box Office: Weekend: $15.56M, Gross: $35.65M
+Watch Trailer Add to Watchlist
+По соображениям совести (2016) Poster
+
+По соображениям совести (2016)
+
+Certificate R 139 min   -   Drama | History | War
+
+8.6/10
+Metascore: 71/100 (45 reviews)
+WWII American Army Medic Desmond T. Doss, who served during the Battle of Okinawa, refuses to kill people, and becomes the first man in American history to win the Medal of Honor without firing a shot.
+Director: Mel Gibson
+Stars: Andrew Garfield, Sam Worthington, Luke Bracey, Teresa Palmer
+Box Office: Weekend: $15.19M, Gross: $53.86M
+Watch Trailer Add to Watchlist
+The Edge of Seventeen (2016) Poster
+
+The Edge of Seventeen (2016)
+
+Certificate R 104 min   -   Comedy | Drama
+
+8/10
+Metascore: 77/100 (37 reviews)
+High-school life gets even more unbearable for Nadine when her best friend, Krista, starts dating her older brother.
+Director: Kelly Fremon Craig
+Stars: Hailee Steinfeld, Haley Lu Richardson, Blake Jenner, Kyra Sedgwick
+Box Office: Weekend: $4.75M, Gross: $11.11M
+Watch Trailer Add to Watchlist
+
+IMDb is your definitive source for discovering the latest new movies now playing in theaters. Find out what movies are opening this week as well as what movies are in the Box Office Top Ten. Before you go to the movie theater, go to IMDb to get the latest buzz, watch the hottest trailers, see photos, find release dates, read reviews, and learn all about the full cast and crew. IMDb is your ultimate source for all new movies, and our "In Theaters" page provides you an easy way to decide what to watch today, tomorrow or this weekend. As you find new movies, add them to your Watchlist. The IMDb Watchlist is a quick and easy way to track and remember all the latest movies that you want to see. Remember, IMDb is your source for new movies.
+
+See all movies in theaters »
+See movies opening next week »
+Share this page:
+
+}
+
+var movies = [movieslist1,movieslist2,movieslist3,megamovielist];
 module.exports=movies;
--- index.js
+++ index.js
@@ -1,52 +1,20 @@
 var exp = require('express');
 var mong= require('mongoose');
-mong.connect('mongodb://unc:landmine@ds117348.mlab.com:17348/itheater')
-var Cat = mong.model('Cat', { name: String });
-
-var kitty = new Cat({ name: 'unc' });
-kitty.save(function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('meow');
-  }
-});
-
-// design
-var movie = mong.model('movies',{name: String, type: String, ratings: String});
-
-// implementation
-var movie1=new movie({name: "MOvie1", type:"horro", ratings:10});
-
-movie1.save(function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('meow');
-  }
-});
-
-var reg2 = {name: "unc"}
-var reg = mong.model('registration',{name: String, email:String, password: String});
-
-var user = new reg({name: "unc", email:"unc@delandev.com", passwor:"landmine"});
-
-user.save(function(err){
-  if (err) {
-    console.log("Oooh" + reg2.name + "you have no idea what you doing");
-  }else{
-    console.log("Good Job Boy");
-  }
-});
+mong.connect('mongodb://unc:landmine@ds117348.mlab.com:17348/itheater');
 var moviedata = require('./api/movies.js');
 app= exp();
 app.set('view engine', 'pug')
 // mongodb://<dbuser>:<dbpassword>@ds117348.mlab.com:17348/itheater
-console.log(moviedata);
+// console.log(moviedata);
 var Router = exp.Router();
+var cityModel = mong.model('cities', {name: String});
 Router.get('/city/:moscow', function(req,res){
   var seasionname=req.params.moscow;
-  res.render('index',{moscow: 'this ' +seasionname+', is good', moviedataInsideTemplate: moviedata})
+  var city = new cityModel({name: seasionname});
+  city.save(function(error){
+    res.render('index',{moscow: 'this ' +seasionname+', is good', moviedataInsideTemplate: moviedata, issave: error ? "record is not save":"Welcome To iTheater"})
+  });
+
 });
 app.use(Router);
 app.use('/public', exp.static('public'));
